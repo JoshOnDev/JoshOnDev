bf09ef05d30c50b156244b30e7356516acc07466
Josh <csw041743@gmail.com>
2026-04-13 16:31:27 +0900

    feat: chapter15 복습

--- section01/chapter15.js
+++ section01/chapter15.js
@@ -4,7 +4,7 @@ let obj1 = {}; // 객체 리터럴 (대부분 사용)
 
 //2. 객체 프로퍼티(객체 속성)
 let person = {
-  name: "정성운",
+  name: "정성운ss",
   age: 27,
   hobby: "moolmeong",
   job: "manager",
@@ -13,9 +13,18 @@ let person = {
   "like dog": true,
 };
 
+let family = {
+  daugther: "정성운성",
+  mother: "김영선",
+  father: "정수호",
+  son: "정성호",
+};
+
+console.log("enter");
+console.log(family.daugther);
+
 //3. 객체 프로퍼티 다루는 법
 //3-1. 특정 프로퍼티에 접근하기 (점 표기법, 괄호 표기법)
-
 let name = person.name;
 
 let age = person["age"];
