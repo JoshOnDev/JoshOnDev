a535da78ca85bbab1300e34c658a00ee79ecf98d
Josh <csw041743@gmail.com>
2026-04-20 23:37:47 +0900

    feat : 반복문 배열과 객체 순회하는 법 학습 및 복습

--- section02/chapter05.js
+++ section02/chapter05.js
@@ -23,3 +23,44 @@ console.log(JSON.stringify(o1) === JSON.stringify(o3));
 
 console.log(o1); // 정성운
 console.log(o3); // 이정환
+
+// 배열과 함수도 사실 객체이다.
+
+// 1. 배열 순회
+let arr = [1, 2, 3];
+
+// 1.1 배열 인덱스
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr2[i]);
+}
+
+let arr2 = [4, 5, 6, 7, 8];
+for (let i = 0; i < arr2.length; i++) {
+  console.log(arr2[i]);
+}
+
+//1.2 for of 반복문
+for (let item of arr) {
+  console.log(item);
+}
+
+// 2. 객체 순회
+
+let person = {
+  name: "정성운",
+  age: 27,
+  hobby: "테니스",
+};
+
+// 2.1 Object.keys 사용
+// -> 객체에서 key 값들만 뽑아서 새로운 배열로 반환
+
+let keys = Object.keys(person);
+for (let key of keys) {
+  console.log(key, person(key));
+}
+
+// 2.2 Object.values
+// -> 객체에서 value 값들만 뽑아서 새로운 배열로 반환
+let values = Object.values(person);
+console.log(values);
